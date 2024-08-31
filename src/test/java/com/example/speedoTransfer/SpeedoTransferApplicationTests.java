7b69624743ef61595b2831193c9a718a127ad8ec
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2024-08-31 15:22:25 +0300

    structure

---
+++ src/test/java/com/example/speedoTransfer/SpeedoTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.speedoTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedoTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
