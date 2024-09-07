043009d39d45a4c21b19202a046fff46bba5f0dc
ibrahim esam <76038894+ibrahimkayroky@users.noreply.github.com>
2024-09-07 16:14:52 +0300

    register
    response - request

--- src/main/java/com/example/speedoTransfer/auth/RegisterResponse.java
+++ src/main/java/com/example/speedoTransfer/auth/RegisterResponse.java
@@ -5,19 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.Date;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class RegisterRequest {
+public class RegisterResponse {
+
     private String name;
     private String email;
-    private String password;
-    private Date birthDate;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
 }
